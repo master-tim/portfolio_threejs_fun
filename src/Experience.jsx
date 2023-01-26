5d965141721ecc2ffb339fcb1d2720b5e174d72c
tim <dzhoroev1@gmail.com>
2023-01-26 16:55:01 +0900

    [add] macbook with website

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { ContactShadows, useGLTF, Environment, Float, PresentationControls } from '@react-three/drei'
+import { ContactShadows, useGLTF, Environment, Float, PresentationControls, Html, Text } from '@react-three/drei'
 
 export default function Experience()
 {
@@ -18,10 +18,37 @@ export default function Experience()
             <Float
                 rotationIntensity={ 0.4 }
             >
+                <rectAreaLight
+                    width={ 2.5 }
+                    height={ 1.65 }
+                    intensity={ 65 }
+                    color={ '#C0C0C0' }
+                    rotation={ [ - 0.1, Math.PI, 0 ] }
+                    position={ [ 0, 0.55, - 1.15 ] }
+                />
                 <primitive 
                     object={ computer.scene } 
                     position-y={ - 1.2 }
-                />
+                >
+                    <Html
+                        transform
+                        wrapperClass="htmlScreen"
+                        distanceFactor={ 1.17 }
+                        position={ [ 0, 1.56, - 1.4 ] }
+                        rotation-x={ - 0.256 }                    
+                    >
+                        <iframe src='https://threejs.org/'/>
+                    </Html>
+                </primitive>
+
+                <Text
+                    font="./bangers-v20-latin-regular.woff"
+                    fontSize={ 1 }
+                    position={ [ 2, 0.75, 0.75 ] }
+                    rotation-y={ - 1.25 }                
+                >
+                    MATIMA
+                </Text>
             </Float>
         </PresentationControls>
 
